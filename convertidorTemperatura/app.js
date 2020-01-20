96dbf5e8048620e94ee00f6b9668cbbe73c5c527
luzcry <58492755+luzcry@users.noreply.github.com>
2020-01-20 18:53:31 -0500

    mostrar en pantalla

--- convertidorTemperatura/app.js
+++ convertidorTemperatura/app.js
@@ -2,4 +2,4 @@ let temperetureinC = parseInt (prompt ("¿Cuál es la temperatura en °C?"));
 
 let temperatureinF = (temperetureinC * 1.8) + 32;
 
-console.log ("la temperatura en Fahrenheit es " + temperatureinF);
+document.write ("la temperatura en Fahrenheit es " + temperatureinF);
