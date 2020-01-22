6e19bd2ba64f86f2bb9ecd52a618f4196ae68716
luzcry <58492755+luzcry@users.noreply.github.com>
2020-01-22 10:50:58 -0500

    coin converter

---
+++ coinCorverter/app.js
@@ -0,0 +1,9 @@
+let dollars= prompt ("¿Cúal es la cantidad en dólares?");
+
+let soles= dollars* 3.25;
+
+let pesosChilenos= dollars*18;
+
+let pesosMexicanos= dollars*660;
+
+console.log(soles, pesosMexicanos, pesosChilenos);
