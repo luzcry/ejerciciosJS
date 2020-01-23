6a69b5a87c813b81ce84e3c1ddc6cc595db4ad63
luzcry <58492755+luzcry@users.noreply.github.com>
2020-01-23 02:37:30 -0500

    funcionesMatematicas

---
+++ funcionesmatematicas/app.js
@@ -0,0 +1,8 @@
+function add(a,b) {
+    return a+b;
+}
+
+const multiply = (a,b) => a*b;
+/// se usan funciones para realizar la suma y multiplicacion de dos parametros
+console.log( multiply  (36325,add(9824, 777)));
+///(36325 * (9824 + 777)
